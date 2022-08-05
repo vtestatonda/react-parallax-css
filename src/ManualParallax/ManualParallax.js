0d2ad0481dcfe06ff39f8e2e0910e666c89f43f2
vtestatonda <victoriotestatonda@gmail.com>
2022-08-05 10:52:46 -0300

    manual parallax

---
+++ src/ManualParallax/ManualParallax.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./manualParallax.css";
+
+export function ManualParallax() {
+  return (
+    <div>
+      <div className="bgimg-1">
+        <div className="caption">
+          <span className="border">SITBA MANDA</span>
+        </div>
+      </div>
+      <div className="section">
+        <h3 className="border">cursos de programacion</h3>
+        <p>
+          RECOMENDACIONES COVID-19 No olvides llevar y utilizar tu
+          barbijo/mascarilla, tapando siempre boca, nariz y mentón. Recordar
+          mantener la higiene. Contarás con alcohol disponible en todo momento.
+          Si presentas síntomas, quedate en tu casa. ¡Nos cuidamos entre todos!
+        </p>
+      </div>
+      <div className="bgimg-2">
+        <div className="caption">
+          <span className="border">y mucho mas</span>
+        </div>
+      </div>
+      <div className="section">
+        <h3 className="border">y mucho mas</h3>
+        <p>
+          RECOMENDACIONES COVID-19 No olvides llevar y utilizar tu
+          barbijo/mascarilla, tapando siempre boca, nariz y mentón. Recordar
+          mantener la higiene. Contarás con alcohol disponible en todo momento.
+          Si presentas síntomas, quedate en tu casa. ¡Nos cuidamos entre todos!
+        </p>
+      </div>
+      <div className="bgimg-3">
+        <div className="caption">
+          <span className="border">www.tincode.es</span>
+        </div>
+      </div>
+    </div>
+  );
+}
